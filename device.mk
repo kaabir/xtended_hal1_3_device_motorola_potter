1924b6118844d7f9d04e6ae85a9fd56e013056da
Vachounet <vachounet@live.fr>
2018-05-21 11:15:50 +0200

    potter: build camera wrapper

--- device.mk
+++ device.mk
@@ -102,6 +102,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
+    camera.msm8953 \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
